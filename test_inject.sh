b8336837f1a8113fec94d4bdb8aeb55a46518f9f
Manuel Ferland <manuel.ferland@yahoo.ca>
2016-09-29 19:41:38 -0400

    add injector 2

--- test_inject.sh
+++ test_inject.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget http://192.168.99.100:32962/solr/admin/collections?action=CREATE&name=film&replicationFactor=3&collection.configName=data_driven
-curl http://192.168.99.100:32962/solr/film/update -H "Content-Type: text/xml" --data-binary @films.xml
+curl -X GET 'http://192.168.99.100:32962/solr/admin/collections?action=CREATE&name=film&replicationFactor=3&numShards=1&collection.configName=data_driven'
+curl -X POST http://192.168.99.100:32962/solr/film/update -H "Content-Type: text/xml" --data-binary @films.xml
 curl http://192.168.99.100:32962/solr/film/update?stream.body=%3Ccommit/%3E
